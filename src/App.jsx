e43285e939cc0f8381d222301fe597295d939de6
mmarrone97 <mmarrone97@gmail.com>
2024-07-24 08:48:10 -0400

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,115 @@
-// src/App.jsx
+import './App.css';
+import { useState, useEffect } from 'react';
+import * as petService from './services/petServices';
+import PetList from './components/PetList';
+import PetDetail from './components/PetDetail';
+import PetForm from './components/PetForm';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [petList, setPetList] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const pets = await petService.index();
+
+        if (pets.error) {
+          throw new Error(pets.error);
+        }
+
+        setPetList(pets);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPets();
+  }, []);
+
+  const updateSelected = (pet) => {
+    setSelected(pet);
+  };
+
+  const handleFormView = (pet) => {
+    if (!pet?.name) setSelected(null);
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const handleAddPet = async (formData) => {
+    try {
+      const newPet = await petService.create(formData);
+
+      if (newPet.error) {
+        throw new Error(newPet.error);
+      }
+
+      setPetList([newPet, ...petList]);
+      setIsFormOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdatePet = async (formData, petId) => {
+    try {
+      const updatedPet = await petService.updatePet(formData, petId);
+  
+      if (updatedPet.error) {
+        throw new Error(updatedPet.error);
+      }
+  
+      const updatedPetList = petList.map((pet) =>
+        pet._id !== updatedPet._id ? pet : updatedPet
+      );
+  
+      setPetList(updatedPetList);
+      setSelected(updatedPet);
+      setIsFormOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleRemovePet = async (petId) => {
+    try {
+      const deletedPet = await petService.deletePet(petId);
+
+      if (deletedPet.error) {
+        throw new Error(deletedPet.error);
+      }
+
+      setPetList(petList.filter((pet) => pet._id !== petId));
+      setSelected(null);
+      setIsFormOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <PetList
+        petList={petList}
+        isFormOpen={isFormOpen}
+        updateSelected={updateSelected}
+        handleFormView={handleFormView}
+      />
+      {isFormOpen ? (
+        <PetForm
+          selected={selected}
+          handleAddPet={handleAddPet}
+          handleUpdatePet={handleUpdatePet}
+        />
+      ) : (
+        <PetDetail
+          selected={selected}
+          handleFormView={handleFormView}
+          handleRemovePet={handleRemovePet}
+        />
+      )}
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/PetDetail.jsx
@@ -0,0 +1,30 @@
+// src/components/PetDetails.jsx
+
+const PetDetails = (props) => {
+  if (!props.selected)
+    return (
+      <div>
+        <h1>NO DETAILS</h1>
+      </div>
+    );
+
+  return (
+    <div>
+      <h1>{props.selected.name}</h1>
+      <h2>Breed: {props.selected.breed}</h2>
+      <h2>
+        Age: {props.selected.age} year{props.selected.age > 1 ? 's' : ''} old
+      </h2>
+      <div>
+        <button onClick={() => props.handleFormView(props.selected)}>
+          Edit
+        </button>
+        <button onClick={() => props.handleRemovePet(props.selected._id)}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PetDetails;
